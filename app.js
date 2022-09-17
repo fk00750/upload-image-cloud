afd37898858d24a4582c133db14ceefe67c97a5f
F.K <94318094+fk00750@users.noreply.github.com>
2022-09-17 20:34:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Router = require("./Routes/main");
-const bodyParser = require('body-parser')
+const mongoose = require("mongoose");
 const app = express();
 
 require("dotenv").config();
@@ -11,4 +11,23 @@ app.use("/api", Router);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running on Port : ${PORT}`));
+const StartServer = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        app.listen(PORT, () => {
+          console.log(
+            `Server is Connected to Database and running on PORT ${PORT}`
+          );
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+StartServer()
